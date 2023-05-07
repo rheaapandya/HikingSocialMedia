7e78c98916ce9fce1eac0b2e4302de2254eee48d
rheaapandya <rhea.pandya@menloschool.org>
2023-05-07 16:09:35 -0700

    work on project

--- database.py
+++ database.py
@@ -22,7 +22,7 @@ db_logger.setLevel(db_logger_log_level)
 
 # Creates the engine to connect with our database
 # TODO: Change the name from "vet" to something relevant
-engine = create_engine("sqlite:///vet.db", echo=False)
+engine = create_engine("sqlite:///hikes.db", echo=False)
 
 # A Scoped Session helps us when creating our apps
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -39,3 +39,5 @@ def init_db():
     # you will have to import them first before calling init_db()
     import models
     Base.metadata.create_all(bind=engine)
+
+    
--- models.py
+++ models.py
@@ -10,4 +10,42 @@ from database import Base
 
 # TODO: Complete your models
 
-class Hike
+class Hike(Base):
+    __tablename__ = "hikes"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    name = Column("name", TEXT, nullable = False)
+    location = Column("location", TEXT, nullable = False)
+    pets = Column("pets", TEXT, nullable = False)
+    weather = Column("weather", TEXT, nullable = False)
+    users = relationship("User", secondary = "userhikes", back_populates = "hikes")
+
+    #Constructor
+    def __init__(self, name, location, pets, weather):
+        self.name = name
+        self.location = location
+        self.pets = pets
+        self.weather = weather
+
+class user(Base):
+    __tablename__ = "users"
+    # Columns
+    username = Column("username", TEXT, primary_key=True)
+    password = Column("password", TEXT, nullable=False)
+    hikes = relationship("Hike", secondary = "userhikes", back_populates = "users")
+    #Constructor
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+class userhike(Base):
+    __tablename__ = "userhikes"
+
+    #Columns
+    id = Column("id", INTEGER, primary_key=True)
+    user_id = Column("user_id", INTEGER, ForeignKey('users.username'))
+    hike_id = Column("hike_id", INTEGER, ForeignKey('hikes.id'))
+
+    
